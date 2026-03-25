b703dc308f23e26a709860cbc508d47a3256846d
zmPolaris <994852191@qq.com>
2026-03-25 09:56:06 +0800

    初始化表结构对象

--- xa-eyre-oracle/src/main/java/cn/xa/eyre/comm/mapper/DiagnosisDictMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.xa.eyre.comm.mapper;
-
-import cn.xa.eyre.comm.domain.DiagnosisDict;
-
-public interface DiagnosisDictMapper {
-    int insert(DiagnosisDict record);
-
-    int insertSelective(DiagnosisDict record);
-}
