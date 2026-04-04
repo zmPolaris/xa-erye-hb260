c3aa36803fceb7276d7c34fa0d658e0695dc5b69
“ouuuuuuu” <mr_baizi@163.com>
2026-04-04 10:04:51 +0800

    ORDERS表修改

---
+++ xa-eyre-oracle/src/main/java/cn/xa/eyre/ordadm/mapper/OrdersMapper.java
@@ -0,0 +1,18 @@
+package cn.xa.eyre.ordadm.mapper;
+
+import cn.xa.eyre.ordadm.domain.Orders;
+import cn.xa.eyre.ordadm.domain.OrdersKey;
+
+public interface OrdersMapper {
+    int deleteByPrimaryKey(OrdersKey key);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    Orders selectByPrimaryKey(OrdersKey key);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
